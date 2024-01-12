ee5f0c4a3a7e773f95acd8c188e0538ba0e62aa0
mrashid007 <mamun_cse007@yahoo.com>
2024-01-12 22:56:26 +0600

    Initial Commit

---
+++ src/_auth/forms/SignupForm.tsx
@@ -0,0 +1,116 @@
+
+import { zodResolver } from "@hookform/resolvers/zod"
+import { Button } from "@/components/ui/button"
+import { useForm } from "react-hook-form"
+import { useToast } from "@/components/ui/use-toast"
+
+import {  Form,  FormControl, FormField,  FormItem,  FormLabel,  FormMessage,} from "@/components/ui/form"
+import { Input } from "@/components/ui/input"
+import { SignupValidation } from "@/lib/validation"
+import { z } from "zod"
+import Loader from "@/components/shared/Loader"
+import { Link } from "react-router-dom"
+import { createUserAccount } from "@/lib/appwrite/api"
+
+
+
+
+const SignupForm = () => {
+  const { toast } = useToast()
+  const isLoading=false;
+
+const form=useForm<z.infer<typeof SignupValidation>>({
+  resolver:zodResolver(SignupValidation),
+  defaultValues:{
+    name:'',
+    username:"",
+    email:'',
+    password:''
+  },
+})
+  async function onSubmit(values: z.infer<typeof SignupValidation>) {
+  const newUser = await createUserAccount(values);
+  
+  if(!newUser)
+  {
+    return toast({
+      title: "Sign up failed. Please try again"
+    })
+  }
+  //console.log(newUser);
+}
+  return (
+    <Form {...form}>
+      <div className="sm:w-420 flex-center flex-col">
+        <img src="/public/assets/images/logo.svg" alt="logo"></img>
+        <h2 className="h3-bold md:h2-bold pt-5 sm:pt-12">Create a new account</h2>
+        <p className="text-light-3 small-medium md:base-regular mt-2"> To use Snapgram,Please enter your details</p>
+      
+    <form onSubmit={form.handleSubmit(onSubmit)} className="flex flex-col gap-5 w-full mt-4">
+      <FormField
+        control={form.control}
+        name="name"
+        render={({ field }) => (
+          <FormItem>
+            <FormLabel>Name</FormLabel>
+            <FormControl>
+              <Input type="text" className="shad-input" {...field} />
+            </FormControl>
+            <FormMessage />
+          </FormItem>
+        )}
+      />
+      <FormField
+        control={form.control}
+        name="username"
+        render={({ field }) => (
+          <FormItem>
+            <FormLabel>Username</FormLabel>
+            <FormControl>
+              <Input type="text" className="shad-input" {...field} />
+            </FormControl>
+            <FormMessage />
+          </FormItem>
+        )}
+      />
+      <FormField
+        control={form.control}
+        name="email"
+        render={({ field }) => (
+          <FormItem>
+            <FormLabel>Email</FormLabel>
+            <FormControl>
+              <Input type="text" className="shad-input" {...field} />
+            </FormControl>
+            <FormMessage />
+          </FormItem>
+        )}
+      />
+      <FormField
+        control={form.control}
+        name="password"
+        render={({ field }) => (
+          <FormItem>
+            <FormLabel>Password</FormLabel>
+            <FormControl>
+              <Input type="text" className="shad-input" {...field} />
+            </FormControl>
+            <FormMessage />
+          </FormItem>
+        )}
+      />
+      <Button type="submit" className="shad-button_primary">
+        {isLoading?(<div className="flex-center gap-2">
+          <Loader></Loader>Loading... </div>):"SIgn up"}
+      </Button>
+      <p className="text-small-regular text-light-2 text-center mt-2">
+          Already have an aoount? 
+          <Link to="/sign-in" className="text-primary-500 text-small-semibold ml-1">Log in</Link>
+      </p>
+    </form>
+    </div>
+  </Form>
+  )
+}
+
+export default SignupForm
---
+++ src/components/shared/Loader.tsx
@@ -0,0 +1,10 @@
+
+const Loader = () => {
+  return (
+    <div className="flex-center w-full">
+      <img src="/public/assets/icons/loader.svg" alt="Loader" width={24} height={24}></img>
+    </div>
+  )
+}
+
+export default Loader
